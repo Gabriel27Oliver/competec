089596953a16422012b8e5d7c0812e532447a99c
Gabriel27Oliver <166921824+Gabriel27Oliver@users.noreply.github.com>
2024-04-13 11:29:07 -0300

    Update Exercicio2.java

--- variaveis/Exercicio2.java
+++ variaveis/Exercicio2.java
@@ -8,6 +8,15 @@ public class Exercicio2 {
     public static void main(String[] args) {
         // Escreva seu código aqui ⬇️
 
+        //exercicio 2
+        
+        float laranja, uva, soma;
+        laranja = 8.50;
+        uva = 10.50;
+        soma = (laranja + uva);
+
+        System.out.print("o valor da soma dos dois é: " + soma);
+
         
     }
 }

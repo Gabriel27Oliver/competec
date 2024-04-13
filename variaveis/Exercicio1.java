6355b037d15768c8acd9cb50fc2ecd0dbad1fa37
Gabriel27Oliver <166921824+Gabriel27Oliver@users.noreply.github.com>
2024-04-13 11:17:13 -0300

    Update Exercicio1.java

--- variaveis/Exercicio1.java
+++ variaveis/Exercicio1.java
@@ -4,10 +4,16 @@
 3. Crie uma variável informando se você tomou café da manhã;
 */
 
+import java.util.Scanner;
+
 public class Exercicio1 {
     public static void main(String[] args) {
         // Escreva seu código aqui ⬇️
 
-
+        //exercicio 1
+        Scanner scan = new Scanner(System.in);
+        char letraN = 'G';
+        int idade = 16;
+        boolean tomouCafe = true;
     }
 }
